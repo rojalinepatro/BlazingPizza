5cd69929d5c35bd4b98754cf3390a59241da4988
rojalinepatro <rojaline.patro@gmail.com>
2024-01-13 18:38:28 +0530

    new project

--- SeedData.cs
+++ SeedData.cs
@@ -10,7 +10,7 @@ namespace BlazingPizza
                 {
                     Name = "Basic Cheese Pizza",
                     Description = "It's cheesy and delicious. Why wouldn't you want one?",
-                    BasePrice = 9.99m,
+                    BasePrice = 499.99m,
                     ImageUrl = "img/pizzas/cheese.jpg",
                 },
                 new PizzaSpecial()
@@ -18,7 +18,7 @@ namespace BlazingPizza
                     Id = 2,
                     Name = "The Baconatorizor",
                     Description = "It has EVERY kind of bacon",
-                    BasePrice = 11.99m,
+                    BasePrice = 375.99m,
                     ImageUrl = "img/pizzas/bacon.jpg",
                 },
                 new PizzaSpecial()
@@ -26,7 +26,7 @@ namespace BlazingPizza
                     Id = 3,
                     Name = "Classic pepperoni",
                     Description = "It's the pizza you grew up with, but Blazing hot!",
-                    BasePrice = 10.50m,
+                    BasePrice = 375.99m,
                     ImageUrl = "img/pizzas/pepperoni.jpg",
                 },
                 new PizzaSpecial()
@@ -34,7 +34,7 @@ namespace BlazingPizza
                     Id = 4,
                     Name = "Buffalo chicken",
                     Description = "Spicy chicken, hot sauce and bleu cheese, guaranteed to warm you up",
-                    BasePrice = 12.75m,
+                    BasePrice = 375.99m,
                     ImageUrl = "img/pizzas/meaty.jpg",
                 },
                 new PizzaSpecial()
@@ -42,7 +42,7 @@ namespace BlazingPizza
                     Id = 5,
                     Name = "Mushroom Lovers",
                     Description = "It has mushrooms. Isn't that obvious?",
-                    BasePrice = 11.00m,
+                    BasePrice = 375.99m,
                     ImageUrl = "img/pizzas/mushroom.jpg",
                 },
                 new PizzaSpecial()
@@ -50,7 +50,7 @@ namespace BlazingPizza
                     Id = 7,
                     Name = "Veggie Delight",
                     Description = "It's like salad, but on a pizza",
-                    BasePrice = 11.50m,
+                    BasePrice = 375.99m,
                     ImageUrl = "img/pizzas/salad.jpg",
                 },
                 new PizzaSpecial()
@@ -58,7 +58,7 @@ namespace BlazingPizza
                     Id = 8,
                     Name = "Margherita",
                     Description = "Traditional Italian pizza with tomatoes and basil",
-                    BasePrice = 9.99m,
+                    BasePrice = 375.99m,
                     ImageUrl = "img/pizzas/margherita.jpg",
                 },
             };
